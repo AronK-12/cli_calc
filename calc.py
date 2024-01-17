abc0b219d01799df50ed75403ac10b9bef245a9d
Aronk-112 <aronkvaszta12@outlook.com>
2024-01-17 09:36:34 +0100

    Added solver. Atm it prioritizes multiplications and divisions, in the order they are from left to right.

--- calc.py
+++ calc.py
@@ -1,5 +1,8 @@
 import argparse
 
+debug_equation:str = "1+2-3*4 / 20"
+debug:bool = True
+
 def split_equation(eq:str) -> list:
     """
     Splits a mathematical equation represented as a string into a list of operands and operators.
@@ -31,13 +34,64 @@ def split_equation(eq:str) -> list:
 
     return eq_list
 
+def solve_operation(num1:float, num2:float, op:str) -> float:
+    match op:
+        case "*":
+            return num1 * num2
+        case "/":
+            return num1 / num2
+        case "+":
+            return num1 + num2
+        case "-":
+            return num1 - num2
+        case _:
+            return ValueError
+
+def solver(eq:list) -> list:
+    new_list:list = eq
+
+    index = 0
+
+    while "*" in new_list or "/" in new_list:
+        number_1:float
+        operand:str
+        number_2:float
+
+        if new_list[index] == "*" or new_list[index] == "/":
+            number_1 = float(new_list[index - 1])
+            number_2 = float(new_list[index + 1])
+            operand = new_list[index]
+
+            result:float = solve_operation(number_1, number_2, operand)
+
+            new_list.pop(index)
+            new_list.pop(index)
+            new_list[index - 1] = str(result)
+            index -= 2
+        index += 1
+
+    return new_list
+
 def main():
-    parser = argparse.ArgumentParser(description='A Simple arithmetic calculator that can solve equations such as [1+2*5]')
-    parser.add_argument('equation', help='can only contain integers and +-*/. No parenthesis.', type=str)
+    if debug == False:
+        parser = argparse.ArgumentParser(description='A Simple arithmetic calculator that can solve equations such as [1+2*5]')
+        parser.add_argument('equation', 
+                            help='''
+                                can only contain integers and +-*/. No parenthesis.\n
+                                If you want to use spaces between symbols and numbers, 
+                                you have to wrap the argument between single or double quotes.''', 
+                            type=str)
 
-    args = parser.parse_args()
+        args = parser.parse_args()
 
-    splitted_equation = split_equation(args.equation)
+        splitted_equation = split_equation(args.equation)
+    else:
+        splitted_equation = split_equation(debug_equation)
+
+    solved = solver(splitted_equation)
+
+    print(solved)
+
 
 
 if __name__ == '__main__':
